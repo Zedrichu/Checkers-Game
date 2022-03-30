14072968bc6d94944ea8cf285fe59ded83489641
Adrian Zvizdenco <adrzvizdencojr@gmail.com>
2022-03-30 19:05:31 +0200

    Improved to follow SOLID

--- Game.java
+++ Game.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 	@__Author --> Created by Adrian Zvizdenco aka Zedrichu
 	@__Date&Time --> Created on 3/2/2022 11:19
 	@__Email --> adrzvizdencojr@gmail.com
-	@__Version --> 1.2
+	@__Version --> 1.4.5
 	@__Status --> Dev
 */
 
@@ -19,18 +19,19 @@ import java.util.Scanner;
 public class Game {
     public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Board.initBoard();
+        Board board = new Board();
+        board.initBoard();
         int counter = 0;
         String key;
         System.out.println("Welcome to our game! \n Press ENTER to advance and Q+ENTER to quit after a turn!");
-        Board.showBoard();
+        board.showBoard();
         while (true) {
             // Take turns in the game
             System.out.print("Let's complete turn number: "+(counter+1));
             key = scan.nextLine();
             if (key.equals("Q") || key.equals("q")) break;
-            Board.updateBoard(counter%2+1);
-            Board.showBoard();
+            board.updateBoard(counter%2+1);
+            board.showBoard();
             counter++;
         }
         System.out.println("Good bye!");
@@ -46,34 +47,34 @@ public class Game {
  * -> The board can be initialized, updated and printed to the screen.
  */
 class Board {
-    static private Square[][] board;
+    private Square[][] board;
     final static private Scanner s = new Scanner(System.in);
 
-    public static Square[][] getBoard() {
+    public Square[][] getBoard() {
         return board;
     }
 
-    public static void setBoard(Square[][] brd) {board = brd;}
+    public void setBoard(Square[][] brd) {board = brd;}
 
     /**
      * Class static method to initialize the board for the start of the game.
      * -> Creates the matrix that holds the squares
      * */
-    public static void initBoard() {
+    public void initBoard() {
         board = new Square[8][8];
         for (int i=0;i<8;i++) {
             for (int j=0;j<8;j++){
                 if (i<3) {
                     if ((i+j)%2==0) {
-                        board[i][j] = new EmptySquare(i,j);
+                        board[i][j] = new Square(i,j);
                     } else {
                         board[i][j] = new BlackPiece(i,j);
                     }
                 } else if (i<5) {
-                    board[i][j] = new EmptySquare(i,j);
+                    board[i][j] = new Square(i,j);
                 } else {
                     if ((i+j)%2==0) {
-                        board[i][j] = new EmptySquare(i,j);
+                        board[i][j] = new Square(i,j);
                     } else {
                         board[i][j] = new WhitePiece(i,j);
                     }
@@ -85,7 +86,7 @@ class Board {
     /**
      * Class static method to print the board game on the screen.
      * */
-    public static void showBoard() {
+    public void showBoard() {
         String indexRow = " x->1 2 3 4 5 6 7 8    ";
         String delimRow = "  +-----------------+  ";
         System.out.println(indexRow);
@@ -93,10 +94,10 @@ class Board {
         for (int i=0;i<8;i++) {
             System.out.print((i+1)+" | ");
             for (int j=0;j<8;j++) {
-                if (board[i][j] instanceof EmptySquare) {
+                if (board[i][j].getFree()) {
                     System.out.print("  ");
-                } else if (board[i][j] instanceof Piece) {
-                    System.out.print(((Piece) board[i][j]).getPlayer().getMarker()+" ");
+                } else {
+                    System.out.print(((Piece) board[i][j]).getColor().getMarker()+" ");
                 }
             }
             System.out.println("| "+(i+1));
@@ -108,25 +109,26 @@ class Board {
     /**
      * Class static method to update the board game after each player's turn.
      * */
-    public static void updateBoard(int turn) {
-        Player p = Player.getSettings(turn);
-        System.out.println("Turn of player: " + p.getName());
+    public void updateBoard(int turn) {
+        Color col = Color.getSettings(turn);
+        assert col != null;
+        System.out.println("\nTurn of player: " + col.getName());
 
         Piece cp;
-        EmptySquare es;
+        Square es;
         Position cppos,espos;
 
         while(true) {
-            cp = getCurrent(p);
+            cp = getCurrent(col);
             if (cp == null) continue;
             cppos = cp.getPos();
             es = getDestination();
             if (es == null) continue;
             espos = es.getPos();
 
-            if (cp.validMove(espos)) {
+            if (cp.validMove(espos, this)) {
                 break;
-            } else if (cp.validJump(espos)) {
+            } else if (cp.validJump(espos, this)) {
                 break;
             } else {
                 System.out.println("Going in the wrong direction!❌");
@@ -147,7 +149,7 @@ class Board {
      * -> Input: a type of enumeration Player
      * -> Output: a Piece object from the board
      * */
-    private static Piece getCurrent(Player p) {
+    private Piece getCurrent(Color col) {
         int pcX;
         int pcY;
 
@@ -161,15 +163,16 @@ class Board {
             System.out.println("Going off board limits! Get back on board!❌");
         }
         Square pc = board[pcY][pcX];
-        if ((p==Player.BLACK? pc instanceof BlackPiece : pc instanceof WhitePiece)) {
-            if (((Piece) pc).checkPossible() && pc.onBoardDig()) {
+        if (pc.getFree()) return null;
+        if (((Piece) pc).getColor()==col) {
+            if (((Piece) pc).checkPossible(this) && pc.onBoardDig()) {
                 return (Piece) board[pcY][pcX];
             } else {
                 System.out.println("All directions are blocked! Try another piece.❌");
             }
         } else {
             System.out.println("Invalid coordinates! There's no player \""+
-                    p.getName()+"\"'s piece there.❌");
+                    col.getName()+"\"'s piece there.❌");
         }
 
         return null;
@@ -179,7 +182,7 @@ class Board {
      * Class static method to obtain a valid piece to be moved according to user input.
      * -> Output: an EmptySquare object denoting the final destination of a piece.
      * */
-    private static EmptySquare getDestination() {
+    private Square getDestination() {
         int dtX;
         int dtY;
         //Request destination position of the piece:
@@ -193,8 +196,8 @@ class Board {
             System.out.println("Going off board limits! Get back on board!❌");
         }
         Square ds = board[dtY][dtX];
-        if (ds instanceof EmptySquare && ds.onBoardDig()) {
-            return (EmptySquare) board[dtY][dtX];
+        if (ds.getFree() && ds.onBoardDig()) {
+            return board[dtY][dtX];
         } else {
             System.out.println("Invalid coordinates! Square occupied or invalid square color.❌");
         }
@@ -210,12 +213,25 @@ class Board {
  * -> Getters and setters for Position are available, as well as methods
  * to check if the Square is on board and if it is on valid positions to hold a Piece.
  */
-abstract class Square {
+class Square {
+    private boolean free = true;
     //Position
     private Position pos;
 
     Square(Position pos){
         this.pos = pos;
+    }
+
+    Square(int xy, int yx){
+        this.pos = new Position(yx, xy);
+    }
+
+    public void setFree(boolean free) {
+        this.free = free;
+    }
+
+    public boolean getFree(){
+        return this.free;
     }
 
     protected void setPos(Position pos) {
@@ -243,13 +259,7 @@ abstract class Square {
         return this.onBoard()
                 && (this.getPos().getX() + this.getPos().getY())%2==1;
     }
-}
 
-/**
- * Class denoting each empty Square on the board, inheriting Square.
- */
-class EmptySquare extends Square{
-    EmptySquare(int y,int x) {super(new Position(x,y));}
 }
 
 /**
@@ -258,54 +268,53 @@ class EmptySquare extends Square{
  * -> Getters and setters for Player are available
  */
 abstract class Piece extends Square{
-    Piece(int y, int x) {super(new Position(x,y));}
-    private Player plr;
+    Piece(int y, int x) {super(new Position(x,y)); this.setFree(false);}
+    private Color color;
 
-    protected Player getPlayer() {
-        return plr;
+    protected Color getColor() {
+        return color;
     }
 
-    protected void setPlayer(Player plr) {
-        this.plr = plr;
+    protected void setPlayer(Color plr) {
+        this.color = plr;
     }
 
     /**
      * Class method to check if a Piece can perform any valid jumps around.
      * -> Output: boolean value if jumps are available
      * */
-    protected boolean checkJumps(){
-        Square[][] board = Board.getBoard();
+    protected boolean checkJumps(Square[][] board){
         int y = getPos().getY();
         int x = getPos().getX();
-        int t = this.getPlayer().getTurn();
+        int t = this.getColor().getTurn();
 
-        return ((x>1 && y>1 && board[y-1][x-1] instanceof Piece &&
-                    ((Piece) board[y-1][x-1]).getPlayer().getTurn()==3-t
-                    && board[y-2][x-2] instanceof EmptySquare) || //Direction: NW
+        return ((x>1 && y>1 && !board[y-1][x-1].getFree() &&
+                    ((Piece) board[y-1][x-1]).getColor().getTurn()==3-t
+                    && board[y-2][x-2].getFree()) || //Direction: NW
 
-                (y>1 && x<6 && board[y-1][x+1] instanceof Piece &&
-                    ((Piece) board[y-1][x+1]).getPlayer().getTurn()==3-t
-                    && board[y-2][x+2] instanceof EmptySquare) || //Direction: NE
+                (y>1 && x<6 && !board[y-1][x+1].getFree() &&
+                    ((Piece) board[y-1][x+1]).getColor().getTurn()==3-t
+                    && board[y-2][x+2].getFree()) || //Direction: NE
 
-                (y<6 && x>1 && board[y+1][x-1] instanceof Piece &&
-                    ((Piece) board[y+1][x-1]).getPlayer().getTurn()==3-t
-                    && board[y+2][x-2] instanceof EmptySquare) || //Direction: SW
+                (y<6 && x>1 && !board[y+1][x-1].getFree() &&
+                    ((Piece) board[y+1][x-1]).getColor().getTurn()==3-t
+                    && board[y+2][x-2].getFree()) || //Direction: SW
 
-                (y<6 && x<6 && board[y+1][x+1] instanceof Piece &&
-                    ((Piece) board[y+1][x+1]).getPlayer().getTurn()==3-t
-                    & board[y+2][x+2] instanceof EmptySquare)); //Direction: SE
+                (y<6 && x<6 && !board[y+1][x+1].getFree() &&
+                    ((Piece) board[y+1][x+1]).getColor().getTurn()==3-t
+                    & board[y+2][x+2].getFree())); //Direction: SE
     }
     /**
      * Abstract class method to check if a Piece can perform any valid moves around.
      * -> Output: boolean value if moves are available
      * */
-    abstract boolean checkPossible();
+    abstract boolean checkPossible(Board board);
 
     /**
      * Class method to check if a Piece can perform a valid move to Position specified.
      * -> Output: boolean value if the move is valid
      * */
-    protected boolean validMove(Position newp) {
+    protected boolean validMove(Position newp, Board board) {
         return Math.abs(getPos().getX()-newp.getX())==1
                 && Math.abs(getPos().getY()- newp.getY())==1;
     }
@@ -315,18 +324,17 @@ abstract class Piece extends Square{
      * -> Input: Position newp, where the Piece might jump to
      * -> Output: boolean value if the jump is valid
      * */
-    protected boolean validJump(Position newp){
-        Square[][] board = Board.getBoard();
+    protected boolean validJump(Position newp, Board board){
         int x1 = getPos().getX();
         int y1 = getPos().getY();
         int x2 = newp.getX();
         int y2 = newp.getY();
         if (Math.abs(x1-x2)==2 && Math.abs(y1-y2)==2
-            && board[y1+(y2-y1)/2][x1+(x2-x1)/2] instanceof Piece
-            && ((Piece) board[y1+(y2-y1)/2][x1+(x2-x1)/2]).getPlayer().
-                getTurn()==3-getPlayer().getTurn()) {
-            board[y1+(y2-y1)/2][x1+(x2-x1)/2] = new EmptySquare(y1+(y2-y1)/2,x1+(x2-x1)/2);
-            Board.setBoard(board);
+            && !board.getBoard()[y1+(y2-y1)/2][x1+(x2-x1)/2].getFree()
+            && ((Piece) board.getBoard()[y1+(y2-y1)/2][x1+(x2-x1)/2]).getColor().
+                getTurn()==3-getColor().getTurn()) {
+            board.getBoard()[y1+(y2-y1)/2][x1+(x2-x1)/2] = new Square(y1+(y2-y1)/2,x1+(x2-x1)/2);
+            board.setBoard(board.getBoard());
             System.out.println("Piece at "+x1+(x2-x1)/2+":"+y1+(y2-y1)/2+" is taken!✓");
             return true;
         }
@@ -339,27 +347,28 @@ abstract class Piece extends Square{
  * -> One can get the marker, turn and name of a Player
  * -> getSettings method to match a player with the input integer (turn)
  * */
-enum Player {
+enum Color {
     BLACK ('#',2,"Black"),
-    WHITE ('●',1,"White"),
-    DEF('_',0,"");
+    WHITE ('●',1,"White");
 
     final private char marker;
     final private int turn;
     final private String name;
 
-    Player (char marker, int turn, String name) {
+    Color (char marker, int turn, String name) {
         this.marker = marker;
         this.turn = turn;
         this.name = name;
     }
 
-    public static Player getSettings(int x) {
-        for (Player col : Player.values()) {
+    public static Color getSettings(int x) {
+        for (Color col : Color.values()) {
             if (col.getTurn()==x) return col;
         }
-        return Player.DEF;
+        return null;
     }
+
+
 
     public char getMarker() {
         return this.marker;
@@ -378,21 +387,21 @@ enum Player {
  * Class denoting each white Piece on the board, inheriting Piece.
  */
 class WhitePiece extends Piece{
-    WhitePiece(int x,int y) {super(x,y); this.setPlayer(Player.WHITE);}
+    WhitePiece(int a,int b) {super(a,b); this.setPlayer(Color.WHITE);}
 
     @Override
-    boolean checkPossible() {
-        Square[][] board = Board.getBoard();
+    boolean checkPossible(Board b) {
+        Square[][] matrix = b.getBoard();
         int y = getPos().getY();
         int x = getPos().getX();
-        return ( (y>=1 && x<=6 && board[y-1][x+1] instanceof EmptySquare)
-                || (y>=1 && x>=1 && board[y-1][x-1] instanceof EmptySquare))
-                || checkJumps();
+        return ( (y>=1 && x<=6 && matrix[y-1][x+1].getFree())
+                || (y>=1 && x>=1 && matrix[y-1][x-1].getFree()))
+                || checkJumps(b.getBoard());
     }
 
     @Override
-    protected boolean validMove(Position newp) {
-        return super.validMove(newp) && newp.getY() < getPos().getY();
+    protected boolean validMove(Position newp, Board b) {
+        return super.validMove(newp,b) && newp.getY() < getPos().getY();
     }
 
 }
@@ -401,21 +410,21 @@ class WhitePiece extends Piece{
  * Class denoting each black Piece on the board, inheriting Piece.
  */
 class BlackPiece extends Piece {
-    BlackPiece(int x,int y) {super(x,y); this.setPlayer(Player.BLACK);}
+    BlackPiece(int a,int b) {super(a,b); this.setPlayer(Color.BLACK);}
 
     @Override
-    boolean checkPossible() {
-        Square[][] board = Board.getBoard();
+    boolean checkPossible(Board b) {
+        Square[][] matrix = b.getBoard();
         int y = getPos().getY();
         int x = getPos().getX();
-        return ( (y<=6 && x<=6 && board[y+1][x+1] instanceof EmptySquare)
-                || (y<=6 && x>=1 && board[y+1][x-1] instanceof EmptySquare))
-                || checkJumps();
+        return ( (y<=6 && x<=6 && matrix[y+1][x+1].getFree())
+                || (y<=6 && x>=1 && matrix[y+1][x-1].getFree()))
+                || checkJumps(b.getBoard());
     }
 
     @Override
-    protected boolean validMove(Position newp) {
-        return super.validMove(newp) && newp.getY() > getPos().getY();
+    protected boolean validMove(Position newp, Board b) {
+        return super.validMove(newp, b) && newp.getY() > getPos().getY();
     }
 
 }
@@ -425,8 +434,8 @@ class BlackPiece extends Piece {
  * -> Keeps 2 integer coordinates with getters & setters, constructor setting the coordinates.
  */
 class Position {
-    private int xcord;
-    private int ycord;
+    final private int xcord;
+    final private int ycord;
 
     Position(int x, int y) {
         this.xcord = x;
@@ -437,16 +446,8 @@ class Position {
         return xcord;
     }
 
-    protected void setX(int xcord) {
-        this.xcord = xcord;
-    }
-
     protected int getY() {
         return ycord;
-    }
-
-    protected void setY(int ycord) {
-        this.ycord = ycord;
     }
 
     @Override
